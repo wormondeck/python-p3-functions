67a681f1c5ed8b5ec2fd7ad5e172ba02b051f0dc
Ronney Arias <ronneyarias@gmail.com>
2024-05-01 13:52:59 -0500

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(1, 2)
 
 def halve(number):
-    pass
+    return number / 2
+
+halve(4)
